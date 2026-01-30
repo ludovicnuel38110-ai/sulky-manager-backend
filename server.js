6a7d57214bb1decbbda056523bba027b99f1ba33
Ludovic Nuel <ludovicnuel38110@gmail.com>
2026-01-30 19:24:15 +0100

    Remove old courses route, keep only races system

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/admin", require("./routes/admin"));
-app.use("/api/courses", require("./routes/courses"));
 app.use("/api/bets", require("./routes/bets"));
 app.use("/api/results", require("./routes/results"));
 
