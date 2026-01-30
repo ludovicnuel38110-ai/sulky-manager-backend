ebe6d1d9fedd82601542c8ef8748c3ac172884a1
Ludovic Nuel <ludovicnuel38110@gmail.com>
2026-01-30 21:33:41 +0100

    fix add GET /api/races list reunions

--- routes/races.js
+++ routes/races.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
-const Race = require("../models/race"); // ton modèle Mongo
+const Race = require("../models/race");
 
 
 /* =========================================
-   GET course complète + partants
+   GET toutes les réunions
+   /api/races
+========================================= */
+
+router.get("/", async (req, res) => {
+  try {
+
+    const reunions = await Race.find();
+
+    res.json(reunions);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Erreur serveur" });
+  }
+});
+
+
+/* =========================================
+   GET une course précise + partants
    /api/races/:id
 ========================================= */
 
